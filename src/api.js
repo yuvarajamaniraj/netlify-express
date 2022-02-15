88bc8f7ad9e3d71d69f22fae53f58eaf3ae6e573
yuvarajamaniraj <yuvarajamaniraj@gmail.com>
2022-02-15 19:31:33 +0530

    up

--- src/api.js
+++ src/api.js
@@ -10,7 +10,7 @@ router.get("/", (req, res) => {
   });
 });
 
-app.use(`/.netlify/functions/api`, router);
+app.use(`/.netlify/codestrix/api`, router);
 
 module.exports = app;
 module.exports.handler = serverless(app);
