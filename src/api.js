03d1e0b6aa17687ab2f1d23b6a7bf433f61cd1e3
yuvarajamaniraj <57003932+TechIdiots@users.noreply.github.com>
2022-02-15 22:28:15 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -180,6 +180,24 @@ router.get("/", (req, res) => {
 });
 
 app.use(cors({origin: 'https://yuvarajamaniraj.netlify.app'}));
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'https://yuvarajamaniraj.netlify.app');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
 app.use(`/.netlify/functions/api`, router);
 
 module.exports = app;
