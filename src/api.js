617d3c38dc8676b95cb165674c36bd10bd1fc49a
yuvarajamaniraj <yuvarajamaniraj@gmail.com>
2022-02-19 00:23:16 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -62,7 +62,7 @@ router.post("/register", (req, res) => {
     }
     else{
       result = JSON.parse(JSON.stringify(result));
-      res.send({Email: email, pass: password, res: result});
+      res.send({emailid: email, passz: password, res: result});
       // var val;
       // for (var key in result) {
       //   val = result[key];
