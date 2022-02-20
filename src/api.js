5aca7a2f2f5e629530dea7885d1d3efba6eaac9c
yuvarajamaniraj <yuvarajamaniraj@gmail.com>
2022-02-20 22:47:39 +0530

    'changed'

--- src/api.js
+++ src/api.js
@@ -77,39 +77,39 @@ router.post("/register", (req, res) => {
   pool.getConnection(function (err, conn) {
     if (err) res.send(err);
     else {
-      // res.send(result)
-      conn.query(`SELECT EXISTS(SELECT * FROM users WHERE email='${email}')`,
-        (err, result) => {
-          if (err) {
-            throw err;
-          }
-          else {
-            result = JSON.parse(JSON.stringify(result));
-            // res.send(result)
-            var val;
-            for (var key in result) {
-              val = result[key];
-              result = val;
-              for (key in result) {
-                val = result[key];
-              }
-              if (val === 0) {
-                conn.query("INSERT INTO users (email, password, verified, otp) VALUES (?,?,?,?)",
-                  [email, password, verified, otp],
-                  (err, result) => {
-                    if (err) {
-                      console.log(err);
-                    } else {
-                      res.send({ user: "created" });
-                    }
-                  })
-              }
-              else {
-                res.send({ userExists: 1 })
-              }
-            }
-          }
-        });
+      res.send(result)
+      // conn.query(`SELECT EXISTS(SELECT * FROM users WHERE email='${email}')`,
+      //   (err, result) => {
+      //     if (err) {
+      //       throw err;
+      //     }
+      //     else {
+      //       result = JSON.parse(JSON.stringify(result));
+      //       // res.send(result)
+      //       var val;
+      //       for (var key in result) {
+      //         val = result[key];
+      //         result = val;
+      //         for (key in result) {
+      //           val = result[key];
+      //         }
+      //         if (val === 0) {
+      //           conn.query("INSERT INTO users (email, password, verified, otp) VALUES (?,?,?,?)",
+      //             [email, password, verified, otp],
+      //             (err, result) => {
+      //               if (err) {
+      //                 console.log(err);
+      //               } else {
+      //                 res.send({ user: "created" });
+      //               }
+      //             })
+      //         }
+      //         else {
+      //           res.send({ userExists: 1 })
+      //         }
+      //       }
+      //     }
+      //   });
     }
   });
 });
