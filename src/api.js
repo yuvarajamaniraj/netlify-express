a70cded6eef538f56f7644f475255e612b9d1fc3
yuvarajamaniraj <yuvarajamaniraj@gmail.com>
2022-02-21 13:18:05 +0530

    'changed'

--- src/api.js
+++ src/api.js
@@ -134,44 +134,44 @@ router.post(("/mail"), (req, res) => {
   })
   const { tomail }=JSON.parse(req.body)
 const newOtp = otpGen(); 
-res.send({email: tomail, OTP: newOtp})  
-// var transporter = nodemailer.createTransport({
-//   service: 'Gmail',
+// res.send({email: tomail, OTP: newOtp})  
+var transporter = nodemailer.createTransport({
+  service: 'Gmail',
 
-//   auth: {
-//     user: 'websitefeedback.codestrix@gmail.com',
-//     pass: 'CodeStrix@123',
-//   }
-// });
+  auth: {
+    user: 'websitefeedback.codestrix@gmail.com',
+    pass: 'CodeStrix@123',
+  }
+});
 
-// var mailOptions = {
-//     from: 'websitefeedback.codestrix@gmail.com',
-//   to: tomail,
-//   subject: 'Verification for Codestrix account',
-//   text: `your OTP for Verification is ${newOtp}`       
-// };
+var mailOptions = {
+    from: 'websitefeedback.codestrix@gmail.com',
+  to: tomail,
+  subject: 'Verification for Codestrix account',
+  text: `your OTP for Verification is ${newOtp}`       
+};
 
-// transporter.sendMail(mailOptions, function(error, info){
-//   if (error) {
-//     res.json({
-//       error: error,
-//       msg: 'fail'
-//     });
-//   } 
-//   else{
-//     pool.query('UPDATE users SET `otp` = ? WHERE `email`=?',[newOtp, tomail],
-//     (err, result) => {
-//       if (err) {
-//         console.log(err);
-//       } else {
-//         console.log("otp stored");
-//       }
-//     })
-//     res.json({
-//       msg: 'success'
-//     })
-//   }
-// });
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    res.json({
+      error: error,
+      msg: 'fail'
+    });
+  } 
+  else{
+    pool.query('UPDATE users SET `otp` = ? WHERE `email`=?',[newOtp, tomail],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("otp stored");
+      }
+    })
+    res.json({
+      msg: 'success'
+    })
+  }
+});
 
 })
 
