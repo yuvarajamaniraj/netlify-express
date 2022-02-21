e2984996cf2d9de974d274fc53efec822b85d294
yuvarajamaniraj <yuvarajamaniraj@gmail.com>
2022-02-21 12:10:09 +0530

    'changed'

--- src/api.js
+++ src/api.js
@@ -65,7 +65,7 @@ router.post("/register", (req, res) => {
     if (err) res.send(err);
     else {
       // res.send(str)
-      conn.query(`SELECT EXISTS (SELECT * FROM users WHERE email='${email}')`,
+      conn.query(`SELECT EXISTS (SELECT * FROM users WHERE email='${req.body.email}')`,
         (err, result) => {
           if (err) {
             throw err;
